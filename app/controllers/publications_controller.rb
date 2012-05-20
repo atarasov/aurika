e802122188e74fe6ba61593ecf7398550abeb8e6
Alexey <lexa-tarasov@yandex.ru>
2012-05-20 20:50:50 +0400

    f

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,10 +1,20 @@
 class PublicationsController < ApplicationController
+  before_filter :get_article
   def index
-    @publications = Publication.all.order("created_at DESC")
+    if params[:article]
+      @publications = Publication.order("created_at DESC").where(:article_id => params[:article])
+    else
+      @publications = Publication.order("created_at DESC").all
+    end
+
   end
 
   def show
     @publication = Publication.find(params[:id])
+  end
+
+  def get_article
+    @articles = Article.all
   end
 
 end
