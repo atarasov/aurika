c1c15b3e5c2a7926ff679ef917b30a9c2f00199e
Alexey <lexa-tarasov@yandex.ru>
2012-05-22 23:31:47 +0400

    hhh

---
+++ app/mailers/support_mailer.rb
@@ -0,0 +1,8 @@
+class SupportMailer < ActionMailer::Base
+  default :from => "support@aurika-curkan.ru"
+
+  def contact_confirmation(contact)
+    @contact = contact
+    mail(:to => "lexa.tarasov@gmail.com", :subject => "Новое приглашение")
+  end
+end
---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,12 @@
+ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.locum.ru",
+    :port                 => 25,
+    :domain               => "aurika-curkan.ru",
+    :user_name            => "support@aurika-curkan.ru",
+    :password             => "fehbrfwehrfy",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+}
+
+ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
